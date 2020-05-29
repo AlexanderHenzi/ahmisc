1080177e3c7a5e5bf558a7310bad7ee4c7de88ff
AlexanderHenzi <alexander.henzi@stat.unibe.ch>
2020-05-29 15:29:32 +0200

    package description

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' ahmisc
+#'
+#' Miscellaneous functions and templates
+#'
+#' @section Rmarkdown templates:
+#' Templates "notebook" (general programming), "report" (share results of a
+#' project) and "consulting" (generate PDF output for consulting cases).
+#'
+#' @name ahmisc-package
+NULL
